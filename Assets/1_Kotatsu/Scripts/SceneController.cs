fdbf5b652f86d4d8f64ec89bc26e446685d945a9
Fuki <meru112mofmof129@gmail.com>
2023-03-25 22:05:59 +0900

    画面遷移

---
+++ Assets/1_Kotatsu/Scripts/SceneController.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ToStart()
+    {
+        //SceneManager.LoadScene("Shika_Start");
+        Debug.Log("ToStart");
+    }
+
+
+    public void ToTheme()
+    {
+        //SceneManager.LoadScene("Theme");
+        Debug.Log("ToTheme");
+    }
+
+    public void ToMain()
+    {
+        //SceneManager.LoadScene("Shika_Main");
+        Debug.Log("ToMain");
+    }
+
+    public void ToResult()
+    {
+        //SceneManager.LoadScene("Shika_Result");
+        Debug.Log("ToResult");
+    }
+}
