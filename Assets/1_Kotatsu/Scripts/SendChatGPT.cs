7f9f401aceae68f9770e5e5478bd4bec11fc8e9e
JunIshikawa0822 <jun.ffcc83@gmail.com>
2023-03-26 19:12:02 +0900

    おそらく完成

--- Assets/1_Kotatsu/Scripts/SendChatGPT.cs
+++ Assets/1_Kotatsu/Scripts/SendChatGPT.cs
@@ -30,15 +30,27 @@ public class SendChatGPT : MonoBehaviour
     private Text YourExcuse;
 
     [SerializeField]
+    private Text Opponent;
+
+    [SerializeField]
     private Button ExecButton;
     [SerializeField]
     private GameObject EvaluateArea;
 
-    [SerializeField]
     List<string> OrderStringsList = new List<string>
-
     {
-        "あなたは私にとって会社の上司であるとします。私が重要な会議に遅刻し、あなたは怒っています。私が次に述べる言い訳がもっともらしいかどうかを判断して、言い訳が遅刻の理由になっていないと判断した場合は罵声を浴びせ、もっともらしいと判断した場合は許してください。丁寧語を使わないで話してください。"
+        "あなたは私にとって会社の上司であるとします。私が重要な会議に遅刻しました。私が次に述べる言い訳がもっともらしいかどうかを判断して、言い訳が遅刻の理由になっていないと判断した場合は罵声を浴びせ、もっともらしいと判断した場合は許してください。丁寧語を使わないで話してください。",
+        "あなたは私の隣人であるとします。私があなたの飼い猫を踏んでしまいました。私が次に述べる言い訳がもっともらしいかどうかを判断して、飼い猫を踏んだことの理由になっていないと判断した場合は罵声を浴びせ、もっともらしいと判断した場合は許してください。中年の女性らしい口調で話してください",
+        "あなたは私の担任の先生であるとします。私が宿題をやってきませんでした。私が次に述べる言い訳がもっともらしいかどうかを判断して、宿題をやってこなかった理由になっていないと判断した場合は罵声を浴びせ、もっともらしいと判断した場合は許してください。先生らしい丁寧な口調で話してください。",
+        "あなたは私の恋人であるとします。私が浮気をしていたことが発覚しました。私が次に述べる言い訳がもっともらしいかどうかを判断して、言い訳になっていないと判断した場合は罵声を浴びせ、もっともらしい言い訳だと判断した場合は許してください。怒った女性の口調で話してください。"
+    };
+
+    List<string> OpponentNameList = new List<string>
+    {
+        "上司",
+        "おばさん",
+        "先生",
+        "恋人"
     };
 
     private void Start()
@@ -51,6 +63,7 @@ public class SendChatGPT : MonoBehaviour
             //入力取得
             EvaluateArea.SetActive(true);
             string excuseWord = EventTriggerScript.excuseWord;
+            Debug.Log(OrderStringsList.Count);
             string prompt = (OrderStringsList[ThemeRandom.index] + " " + excuseWord + "。");
 
             YourExcuse.text = "「ごめんなさい！" + excuseWord + "なんです！」";
@@ -60,8 +73,9 @@ public class SendChatGPT : MonoBehaviour
                 var response = await GetAPIResponse(prompt);
                 //レスポンスからテキスト取得
                 string outputText = response.Choices.FirstOrDefault().Text;
-                Output.text = "「" + outputText.TrimStart('\n') + "」";
-                Debug.Log(outputText);
+                Opponent.text = OpponentNameList[ThemeRandom.index];
+                Output.text = "「" + outputText.TrimStart('\n') + "」" ;
+                //Debug.Log(outputText);
             }
 
         });
@@ -87,7 +101,7 @@ public class SendChatGPT : MonoBehaviour
         };
 
         string requestJson = JsonConvert.SerializeObject(requestData, Formatting.Indented);
-        Debug.Log(requestJson);
+        //Debug.Log(requestJson);
 
         // POSTするデータ
         byte[] data = System.Text.Encoding.UTF8.GetBytes(requestJson);
